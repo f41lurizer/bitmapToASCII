6917ed60fdcacab091cb70733249887bb6f0ca35
f41lurizer <forumf41l@gmail.com>
2015-04-26 01:02:18 -0500

    working version using external library

--- bmp.cpp
+++ bmp.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <fstream>
-#include "qdbmp.h"
+#include "bitmap/bitmap_image.hpp"
+
 using namespace std;
 
-int main(int argc, char* file[])
+int main(int argc, char* argv[])
 {
-    BMP* bmp = BMP_ReadFile("fgt");
-    if(BMP_GetError() != BMP_OK)
-        cout << "an eror has occured with opening the image";
     
+    string fileName =  argv[2];    
+    bitmap_image image(fileName);
+    if(!image)
+    {
+        cout << "error opening input file";
+        return 1;
+    }
+
+    unsigned char red, green, blue;
+    const unsigned int height = image.height(), width = image.width();
+    for(int h = 0; h < height; h++)
+    {
+        for(int w = 0; w < width; w++)
+        {
+           image.get_pixel(w, h, red, green, blue);
+           const char *outputChar = ((red <= 200 || blue <= 200 || green <= 200)?((red <= 100 || blue <= 100 || green <= 100)?"O":"o"):".");
+           //const char *outputChar = (red <= 200 || blue <= 200 || green <= 200)?"O":"o";
+           cout << *outputChar;
+        }        
+        cout << endl;
+    }
 }
